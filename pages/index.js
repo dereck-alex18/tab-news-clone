b1b13e754f1cfe13861a5737a5b9fdb868e7f62e
Dereck Portela <dereck_alexander@outlook.com>
2024-12-11 12:26:06 -0300

    change div for h1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <div>Hello World</div>
+    return (
+        <>
+            <h1>Rita, Jesus te ama e eu tambem! ❤️</h1>
+            <h3>ass: Dereck.</h3>
+        </>
+    )
 }
 
 export default Home;
