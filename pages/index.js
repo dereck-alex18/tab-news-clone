879b300f5523f35041f3dd10cdfa83ea27dc93f3
Dereck Portela <38384320+dereck-alex18@users.noreply.github.com>
2025-01-16 21:46:39 +0000

    add editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home(){
-    return (
-        <>
-            <h1>Rita, Jesus te ama e eu tambem! ❤️</h1>
-            <h3>ass: Dereck.</h3>
-        </>
+  return (
+    <>
+      <h1>Rita, Jesus te ama e eu tambem! ❤️</h1>
+      <h3>ass: Dereck.</h3>
+    </>
     )
 }
 
