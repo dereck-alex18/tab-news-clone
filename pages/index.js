3c06e8bd09116f3d752ce2111a1d7db951326439
Dereck Portela <38384320+dereck-alex18@users.noreply.github.com>
2025-01-16 22:09:47 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-function Home(){
+function Home() {
   return (
     <>
       <h1>Rita, Jesus te ama e eu tambem! ❤️</h1>
       <h3>ass: Dereck.</h3>
     </>
-    )
+  );
 }
 
 export default Home;
